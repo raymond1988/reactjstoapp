37abbdfc9bb6e618142271f82750a9e285046644
Raymond <raymanoharan@gmail.com>
2022-01-25 21:35:15 -0500

    removed usestate

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext} from 'react'
 import { GlobalContext } from '../context/GlobalProvider'
 import ToDoItem from './ToDoItem'
 import AddToDoForm from './AddToDoForm'
